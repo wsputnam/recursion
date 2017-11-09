f3d95fce3840885642574b4a9538c033c737ad30
Your Name <williamputnamjr@gmail.com>
2017-11-09 11:44:56 -0700

    tried for all types of data need to add boolean and fix extra quotation marks

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,24 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'undefined') {
+  	return null;
+  }
+  if (typeof obj === 'null') {
+  	return 'null';
+  }
+  if (typeof obj === 'number' || typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+  if (typeof obj === 'object' && Array.isArray(obj)) {
+  	return '[' + obj.map(function(elem) {
+      return stringifyJSON(elem);
+  	}).join(',')+ ']';
+  } else if (typeof obj === 'object' && Array.isArray(obj) === false) {
+    var output = [];
+    for (var keys in obj) {
+      output.push('"' + key + '":' + '"' + val + '"');
+    }
+    return '{' + output.join(',') + '}';
+  }
 };
