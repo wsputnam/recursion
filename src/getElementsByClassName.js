f452a2de6d947d3c67b28549ef136f8ea58b6303
Your Name <williamputnamjr@gmail.com>
2017-11-09 12:25:59 -0700

    trying to come up with a base case for getElementsByClassName recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,7 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  if (className )
 };
