45c8b6a57458624d9dabbfa98dbc998a1c14dafe
Your Name <williamputnamjr@gmail.com>
2017-11-09 13:56:41 -0700

    just writing every idea I can think of in comments

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -11,6 +11,20 @@ var getElementsByClassName = function(className) {
   if (element).hasClass(className) {
     output.push(element);
   }
+
+  // or
+
+  if (typeof element === 'object' && element.hasOwnProperty(className)) {
+  	output.push(element);
+  }
+
+  // need to account for elements with more than one class when solving
+
+
+  // need to account for child nodes and nested html structures with recursion
+
+
+  
  // if (className exists in html file, could we test with index of?)
   // could use filter, but we don't know what we would be using the function on (document, id, etc.)
   return output;
