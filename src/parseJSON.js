0acc929c292d56558c27db44cd7be9af65202b76
Your Name <williamputnamjr@gmail.com>
2017-11-10 12:55:30 -0700

    starting plan and pseudo code for parse json

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,10 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+  // takes a string from a web server and converts it to a JS object
+  var arr = json.split(' '); // if we do every character, it will be hard to notice booleans and numbers
+  arr.forEach(function(char) {
+    // if the character is a certain type of character, convert that character to something else
+  });
 };
